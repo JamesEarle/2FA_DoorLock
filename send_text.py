c78970e0cec8bbcaa2748dc697d4fe907250a4d5
Kevin Leung <keleung@microsoft.com>
2016-09-17 16:49:14 -0500

    refactored, readme (fritzing + logic)

--- send_text.py
+++ send_text.py
@@ -1,17 +1,24 @@
+#################################### 
+# IoT 2-Factor-Auth Door Lock (Facial + Voice Recognition)
+# Kevin Leung @KSLHacks (Git: KSLHacks)
+# James Earle @ItsJamesIRL (Git: JamesEarle)
+# 9/12/2016
+
+########### Python 2.7 #############
 from twilio import TwilioRestException
 from twilio.rest import TwilioRestClient
 
 def run():
-    account_sid = "ACcb92d2f1964f41bf5e02791cfc580897"
-    auth_token  = "9ae1564c746a9798044e43b467e3ac0b"
+    account_sid = "{Twilio_sid}"
+    auth_token  = "Twilio_token"
 
     client = TwilioRestClient(account_sid, auth_token)
 
     print("Sending message...")
 
     try:
-        message = client.messages.create(body="Uh oh! Looks like someone is trying to get into your locked door.", from_="+14069480046", to="+14245422875")
-        # message = client.messages.create(body="Uh oh! Looks like someone is trying to get into your locked door.", from_="+14069480046", to="+14245422875", media_url="https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg")
+        message = client.messages.create(body="Uh oh! Looks like someone is trying to get into your locked door.", from_="{phoneNumber}", to="{phoneNumber}")
+        # message = client.messages.create(body="Uh oh! Looks like someone is trying to get into your locked door.", from_="{phoneNumber}", to="{phoneNumber}", media_url="https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg")
     except TwilioRestException as e:
         print(e)
 
