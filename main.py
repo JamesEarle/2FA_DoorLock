31c472d98a1e6297061062b3eb512ad2cc13e029
Kevin Leung <leung.kevin23@gmail.com>
2017-02-19 14:44:39 -0600

    refactored for audio functionality

--- main.py
+++ main.py
@@ -6,9 +6,9 @@
 
 ########### Python 2.7 #############
 #!/usr/bin/python
-import send_text as st
-# Can't use this import, not supported on the Raspberry Pi without certain hardware
-#import audio as a
+# send_text option feature.. already implemented
+# import send_text as st
+import audio as a
 import faceRecognition as f
 import RPi.GPIO as GPIO
 import os
@@ -19,7 +19,7 @@ GPIO.setwarnings(False)
 
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(18, GPIO.OUT) # face green
-GPIO.setup(22, GPIO.OUT)  # face red
+GPIO.setup(22, GPIO.OUT) # face red
 GPIO.setup(17, GPIO.OUT) # voice green
 GPIO.setup(27, GPIO.OUT) # voice red
 
@@ -78,9 +78,12 @@ try:
         # Check for state change (button press)
         if(new_input != old_input):
             print("BUTTON PRESS")
+            # flash both face green and red to signify picture in progress
 	        GPIO.output(18, GPIO.HIGH)
-            time.sleep(0.5)
+            GPIO.output(22, GPIO.HIGH)
+            time.sleep(1)
             GPIO.output(18, GPIO.LOW)
+            GPIO.output(22, GPIO.LOW)
 
             # Take a photo using the Raspberry Pi camera
             os.system("raspistill -o output.jpg -q 40")
@@ -88,27 +91,48 @@ try:
             # Use the photo we just took and verify with API
             print("Verifying face...")
             if(f.faceVerify()):
-                print("Success!")
-                # Face is a success, now perform voice
+                print("Success - Face!")
+                # Face is a success - signify by lighting green face
                 GPIO.output(18, GPIO.HIGH)
                 time.sleep(1)
-                break
-                # We don't have necessary hardware to implement voice
-                # but we did implement the logic in audio.py
 
-                # if(a.voiceVerify()):
-                #     GPIO.output(321, GPIO.HIGH)
-                #     both successful, flash lights
+                # Now test voice
+                # flash both voice green and red to signify voice in progress
+                GPIO.output(17, GPIO.HIGH)
+                GPIO.output(27, GPIO.HIGH)
+                time.sleep(1)
+                GPIO.output(17, GPIO.LOW)
+                GPIO.output(27, GPIO.LOW)
+
+                if(a.voiceVerify()):
+                    print("Success - Voice!")
+                    GPIO.output(17, GPIO.HIGH)
+                    break
+                else:
+                    print("Failure - Voice!")
+                    GPIO.output(27, GPIO.HIGH)
+                    break
             else:
                 # Failed to verify, send text and return
-                print("Failure!")
-                GPIO.output(22, GPIO.LOW)
+                print("Failure - Face!")
                 GPIO.output(22, GPIO.HIGH)
                 time.sleep(3)
-                st.run()
+                # texting functionality commented out for this demo - refer to implementation for details
+                #st.run()
                 break
             time.sleep(0.2)
 finally:
+
+    # hold this finished position until button is pressed again to end the program
+    old_input = GPIO.input(23)
+    while True:
+        new_input = GPIO.input(23)
+
+        # Check for state change (button press)
+        if(new_input != old_input):
+            break
+        else:
+
     # Turn of LEDs
     GPIO.output(18, GPIO.LOW)
     GPIO.output(22, GPIO.LOW)
